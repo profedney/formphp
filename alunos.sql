35d7d8ee2a420af5c3398c637a104888323924ce
profedney <111137546+profedney@users.noreply.github.com>
2023-03-17 10:22:14 -0300

    atualizado db

--- alunos.sql
+++ alunos.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 14-Mar-2023 às 15:07
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Host: localhost:3306
+-- Tempo de geração: 17-Mar-2023 às 13:21
+-- Versão do servidor: 10.5.16-MariaDB
+-- versão do PHP: 7.3.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `escola`
+-- Banco de dados: `id19705063_db1`
 --
 
 -- --------------------------------------------------------
@@ -28,23 +29,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `alunos` (
-  `ID` int(11) NOT NULL,
   `nome` varchar(20) DEFAULT NULL,
   `sobrenome` varchar(40) DEFAULT NULL,
-  `turma` varchar(3) DEFAULT NULL
+  `turma` varchar(3) DEFAULT NULL,
+  `id1` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `alunos`
 --
 
-INSERT INTO `alunos` (`ID`, `nome`, `sobrenome`, `turma`) VALUES
-(0, 'Kaique', 'Santos', '3ºB'),
-(1, 'Alisson', 'Evangelista', '3ºB'),
-(2, 'Eduardo', 'Marques', '3ºB'),
-(3, 'Felipe', 'Alexandre', '3ºB'),
-(4, 'Felipe', 'Moura', '3ºB'),
-(5, 'Felipe', 'Soares', '3ºB');
+INSERT INTO `alunos` (`nome`, `sobrenome`, `turma`, `id1`) VALUES
+('Kaique', 'Santos', '3ºB', 1),
+('Alisson', 'Evangelista', '3ºB', 2),
+('Eduardo', 'Marques', '3ºB', 3),
+('Felipe', 'Alexandre', '3ºB', 4),
+('Felipe', 'Moura', '3ºB', 5),
+('Felipe', 'Soares', '3ºB', 6),
+('Edney', 'Rossi', '3B', 7),
+('Edney', 'Rossi', '3B', 8),
+('Edney', 'Rossi', '3B', 9);
 
 --
 -- Índices para tabelas despejadas
@@ -54,7 +58,17 @@ INSERT INTO `alunos` (`ID`, `nome`, `sobrenome`, `turma`) VALUES
 -- Índices para tabela `alunos`
 --
 ALTER TABLE `alunos`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id1`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  MODIFY `id1` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
